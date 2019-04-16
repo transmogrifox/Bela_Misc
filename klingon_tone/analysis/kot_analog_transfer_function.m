1fd46d26c0031bff4d0e34b74ae1bff819e2beb9
Transmogrifox <transmogrifox@cackleberrypines.net>
2019-04-15 22:40:53 -0700

    Finish derivation of Bilinear Transformation for the Tonestack

--- klingon_tone/analysis/kot_analog_transfer_function.m
+++ klingon_tone/analysis/kot_analog_transfer_function.m
@@ -8,21 +8,32 @@ u = 1e-6;
 n = 1e-9;
 
 % Frequency variables
-f = 1:20000;
+fs = 44100;
+f = 1:fs;
 w = 2*pi*f;
 s = j.*w;
+
+% DSP
+z1 = e.^(-s/fs);
+z2 = e.^(-2*s/fs);
+kk = 2*fs;
+
+% Pre-warping
+wk = 2*pi*1*k;
+%kk = wk/(tan(wk/(2*fs)));
+sz = kk*(1 - z1)./(1 + z1);
 
 % Tonestack components
 ri = 1*k;         % Feed resistor from final op amp output
 rtone = 25*k;     % Tone Pot
-apos = 0.5; % Tone Pot position, primary leg resistance ratio
-bpos = 1 - apos;   % Alternate pot leg resistance ratio
+apos = 0.31;      % Tone Pot position, primary leg resistance ratio
+bpos = 1 - apos;  % Alternate pot leg resistance ratio
 rboost = 50*k;    % Boost pot
 bstpos = 1.0;     % Boost pot position resistance ratio
 
 % Components as labeled in derivation
 ra = ri + apos*rtone;
-rb = bpos*rtone;
+rb = bpos*rtone + rtone/fs; % This sets one of the zeros, so it needs to avoid going to zero
 rc = 6.8*k;
 rd = rboost*bstpos;
 ro = 100*k*1000*k/(100*k + 1000*k);
@@ -62,6 +73,7 @@ vxvi = gvx.*(s.*s + A1*s + A0)./(s.*s + B1*s + B0);  % Final expression for Vx/V
 %vovx = (s.*c2*ro*rd + ro)./(s.*c2*(ro*rd+(ro+rd)*rc) + rc + ro);
 gvo = (c2*ro*rd)/(c2*(ro*rd+(ro+rd)*rc));
 %vovx = gvo.*(s + 1/(c2*rd ))./(s + (rc + ro)/(c2*(ro*rd+(ro+rd)*rc)));
+
 X0 = 1/(c2*rd );
 Y0 = (rc + ro)/(c2*(ro*rd+(ro+rd)*rc));
 
@@ -81,8 +93,42 @@ Hs = vxvi.*vovx;  % With more algebra the entire expression could be reduced to
                   % will require more computational cost than strictly necessary
                   % for this filter implementation. 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Bilinear Transform  %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% ********************************** Vx/Vi ********************************** %
+% ************************** Aggregate Coefficients ************************* %
+gblt = ((A0 + kk*A1 + kk.*kk)./(B0 + kk*B1 +kk*kk));
+gdsp = gvx.*gblt;
+A1blt = ((2*A0-2*kk*kk)./(A0+kk*A1+kk*kk));
+A2blt = ((A0-kk*A1+kk*kk)./(A0+kk*A1+kk*kk));
+B1blt = ((2*B0-2*kk*kk)./(B0+kk*B1+kk*kk));
+B2blt = ((B0-kk.*B1+kk.*kk)./(B0+kk.*B1+kk.*kk));
+
+% **************************** Stage-1 DSP Filter *************************** %
+
+vxviblt = gdsp.*(A2blt.*z2 + A1blt.*z1 + 1) ./ ( B2blt.*z2 + B1blt.*z1 + 1);
+
+% ********************************** Vo/Vx ********************************** %
+% ************************** Aggregate Coefficients ************************* %
+
+g2blt = (X0 + kk)./(Y0 + kk);
+gdsp2 = gvo.*g2blt;
+X0blt = (X0 - kk)./(X0 + kk);
+Y0blt = (Y0 - kk)./(Y0 + kk);
+
+% **************************** Stage-2 DSP Filter *************************** %
+
+vovxblt = gdsp2*(X0blt*z1 + 1)./(Y0blt*z1 + 1);
+
+%
+% ***************** Cascade Stage-1 and Stage-2 DSP Filters ***************** %
+%
+
+Hz = vxviblt.*vovxblt;              
+
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%% Check Transfer Functions  %%%%%%%%%%%%%%%%%%%%%%%%%
+%
 
 % Check algebra steps used to simplify transfer functions
 % Below MATLAB/Octave can handle the complex impedances numerically,
@@ -98,11 +144,17 @@ vovxck = zp./(zp + rc);
 
 Hs2 = vxvick.*vovxck;
 
+% BLT Check
+%vxviblt = gvx.*(sz.*sz + A1*sz + A0)./(sz.*sz + B1*sz + B0);
+%vovxblt = gvo*(sz + X0)./(sz + Y0);
+%Hz = vxviblt.*vovxblt;
+
 % Plot
 semilogx(f, 20*log10(Hs), 'r')
 hold on
 semilogx(f, 20*log10(Hs2*0.997), 'g') % when the traces overlap, the 0.997 factor makes them side-by-side visible
-
+semilogx(f, 20*log10(Hz), 'm') 
+hold off
 
 
 
